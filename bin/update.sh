a3905ffca4a039ac97d0afa93582c3afd72214f7
李鑫 <micro-tech@foxmail.com>
2015-03-11 14:03:23 +0800

    Fix compatibility on Windows

--- bin/update.sh
+++ bin/update.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 cd `dirname $0`/..
 tmp=/tmp/gh-pages-miemie-board
+asserts="font-awesome fonts img"
 mkdir -p $tmp
 rm -rf $tmp/* >/dev/null 2>&1
 grunt
-cp -rL dist/* $tmp
+cp -rL dist/* $tmp >/dev/null 2>&1
+cd public
+cp -rf $asserts $tmp >/dev/null 2>&1
 git checkout gh-pages
-cp -rf $tmp/* .
+cp -rf $tmp/* ..
