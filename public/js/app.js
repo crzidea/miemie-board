b3a207ff7895c1361bcbb51f128d618152819dab
Li Xin <jackxinli@sohu-inc.com>
2015-03-16 23:03:46 +0800

    Add animation

--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,73 @@
-var db = openDatabase('scoreboard', '', '', 1024 * 1024);
+var logContainer = document.querySelector('#history .container.logs');
+var scoreElem = document.querySelector('.score-total strong');
+var scoreProgressBar = document.querySelector('#scores .progress-bar');
+var selectElem = document.querySelector('.form-score select');
+var submitElem = document.querySelector('.form-score button');
+var db = openDatabase('scoreboard', '0.0.1', '', 1024 * 1024);
 db.transaction(function(tx) {
-  tx.executeSql('CREATE TABLE IF NOT EXISTS histories (date DATE, score INT)');
-  tx.executeSql('SELECT SUM(score) sum FROM histories', null,
+  tx.executeSql('CREATE TABLE IF NOT EXISTS histories (date, score)');
+  tx.executeSql('SELECT * FROM histories ORDER BY rowid DESC', null,
                 function(tx, result) {
-                  var sum = result.rows.item(0).sum;
-                  var elem = document.querySelector('.score-total strong');
-                  elem.innerHTML = sum;
-                  var progressBar = document.querySelector('#scores .progress-bar');
-                  progressBar.style.width = (sum / 10) + '%'
+                  var logs = '';
+                  for (var i = 0, l = result.rows.length; i < l; i ++) {
+                    var item = result.rows.item(i);
+                    logs += renderLog(item);
+                    updateRow(item);
+                  }
+                  logContainer.innerHTML += logs;
                 });
-  tx.executeSql('SELECT * FROM histories ORDER BY rowid DESC', null, function(tx, result) {
-    var logs = '';
-    for (var i = 0, l = result.rows.length; i < l; i ++) {
-      var v = result.rows.item(i);
-      var date = new Date(v.date);
-      var dateStr = date.getFullYear() +
-        '-' + (date.getMonth() || 12) +
-        '-' + date.getDate();
-      logs += '<div class="row log"><div class="col-xs-6 date">' +
-        dateStr + '</div><div class="col-xs-6 score">获得积分<strong>' +
-        v.score + '</strong></div></div>';
+  function updateRow(item) {
+    if (!/^\d+$/.test(item.date)) {
+      var date  = +new Date(item.date);
+      tx.executeSql('UPDATE histories SET date=? WHERE date=?',
+                    [date, item.date]);
     }
-    var container = document.querySelector('#history .container');
-    container.innerHTML += logs;
-  });
+  }
+  updateScore(tx);
 });
+
+function updateScore(tx) {
+  tx.executeSql('SELECT SUM(score) sum FROM histories', null,
+    function(tx, result) {
+      var sum = result.rows.item(0).sum;
+      scoreElem.innerHTML = sum;
+      scoreProgressBar.style.width = (sum / 10) + '%'
+    }
+  );
+}
+
+function renderLog(log) {
+  var date = new Date(log.date);
+  var dateStr = date.getFullYear() +
+    '-' + (date.getMonth() || 12) +
+    '-' + date.getDate();
+  return '<div class="row log"><div class="col-xs-6 date">' +
+    dateStr + '</div><div class="col-xs-6 score">' +
+    (0 < log.score ? '获得' : '扣除') + '积分<strong>' +
+    Math.abs(log.score) + '</strong></div></div>';
+}
 
 function addScore(event, form) {
   event.preventDefault();
-  var select = document.querySelector('.form-score select');
+  var colorEnable = '#95a5a6';
+  var colorDisable = 'whitesmoke';
+  submitElem.style.background = colorDisable;
+  submitElem.disabled = true;
   db.transaction(function(tx) {
-    tx.executeSql('INSERT INTO histories (date, score) values (?, ?)',
-                  [ new Date, select.value],
+    var date = new Date();
+    tx.executeSql('INSERT INTO histories (date, score) VALUES (?, ?)',
+                  [ date, selectElem.value],
                   function(tx, result) {
+                    setTimeout(function () {
+                      submitElem.style.background = colorEnable;
+                      submitElem.disabled = false;
+                    }, 1000);
+
+                    updateScore(tx);
+                    logContainer.innerHTML = renderLog({
+                      date: date,
+                      score: selectElem.value
+                    }) + logContainer.innerHTML;
                   });
   });
 }
